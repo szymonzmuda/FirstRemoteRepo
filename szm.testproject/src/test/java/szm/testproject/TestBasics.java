f3af774bb471315987645714d010ef25694cfa38
Szymon Zmuda <szm@commit.pl>
2022-12-01 16:42:35 +0100

    Basics tests first commit

---
+++ szm.testproject/src/test/java/szm/testproject/TestBasics.java
@@ -0,0 +1,11 @@
+package szm.testproject;
+
+import org.testng.annotations.Test;
+
+public class TestBasics {
+
+	@Test
+	public void testBasic() {
+		System.out.println("... testing Hello World ...");
+	}
+}
